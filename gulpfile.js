07eb392a0082e4253861fe3144ca221a6401f813
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-12-07 17:40:45 +0700

    update []

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var octo = require('@octopusdeploy/gulp-octo');
 var gulp = require("gulp");
 
 gulp.task('publish', function () { // ['build']
-	var apiKey = process.argv[0];
-	var host = process.argv[1];
+	var apiKey = process.argv[4];
+	var host = process.argv[6];
 	console.log(host);
 	console.log(apiKey);
   // return gulp.src(['**/*', '!gulpfile.js'])
