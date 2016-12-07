fb474a51b48a96183e82c5fcd516bbe4ab0fec73
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-12-07 17:33:32 +0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require("gulp");
 gulp.task('publish', function () { // ['build']
 	var apiKey = process.argv[0];
 	var host = process.argv[1];
+	console.log(host);
+	console.log(apiKey);
   // return gulp.src(['**/*', '!gulpfile.js'])
       // .pipe(octo.pack())
       // .pipe(octo.push({apiKey: '%OctopusAPIKey%', host: '%OctopusHost%'}));
